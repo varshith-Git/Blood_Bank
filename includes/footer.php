4ff9f170f913668ec78e3800b7fba42c1432db7a
varshith-Git <varshith.gudur17@gmail.com>
2021-04-04 12:17:28 +0530

    updtae on 4/4/2021

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+  
+  
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white">DESIGN BY VARSHITH => BloodBank & Donor Management System 2020</p>
+        </div>
+        <!-- /.container -->
+    </footer>
---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>BloodBank & Donor Management System</title>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/modern-business.css" rel="stylesheet">
+    <style>
+    .navbar-toggler {
+        z-index: 1;
+    }
+    
+    @media (max-width: 576px) {
+        nav > .container {
+            width: 100%;
+        }
+    }
+    .carousel-item.active,
+    .carousel-item-next,
+    .carousel-item-prev {
+        display: block;
+    }
+    </style>
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+<?php include('includes/header.php');?>
+<?php include('includes/slider.php');?>
+   
+    <!-- Page Content -->
+    <div class="container">
+
+        <h1 class="my-4">Welcome to BloodBank & Donor Management System</h1>
+
+       
+
+        <!-- Portfolio Section -->
+        <h2>Some of the Donars::</h2>
+
+        <div class="row">
+                   <?php 
+$status=1;
+$sql = "SELECT * from tblblooddonars where status=:status order by rand() limit 6";
+$query = $dbh -> prepare($sql);
+$query->bindParam(':status',$status,PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{ ?>
+
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top img-fluid" src="images/blood-donor.jpg" alt="" ></a>
+                    <div class="card-block">
+                        <h4 class="card-title"><a href="#"><?php echo htmlentities($result->FullName);?></a></h4>
+<p class="card-text"><b>  Gender :</b> <?php echo htmlentities($result->Gender);?></p>
+<p class="card-text"><b>Blood Group :</b> <?php echo htmlentities($result->BloodGroup);?></p>
+
+                    </div>
+                </div>
+            </div>
+
+            <?php }} ?>
+       
+            </div>
+            <div class="col-md-4">
+                <a class="btn btn-lg btn-secondary btn-block" href="become-donar.php">Become a Donar</a>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+  <?php include('includes/footer.php');?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/tether/tether.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
